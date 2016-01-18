d3bceb8cbc37ea6a5e45962e4867120233b87606
Ishan Dindorkar <ishan.dindorkar@yahoo.com>
2016-01-17 20:32:35 -0600

    first commit

---
+++ LinkedList/src/com/pract/linkedlist/InsertList.java
@@ -0,0 +1,72 @@
+package com.pract.linkedlist;
+
+class Node{
+	
+	private int data;
+	private Node next;
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	
+	public static Node create(int data){
+		Node n = new Node();
+		n.setData(data);
+		n.setNext(null);
+		return n;
+	}
+	
+	public static Node insert(Node head, Node node, int pos){
+		if(pos == 1){
+			head.setNext(node);
+		}
+		else{
+			Node temp = new Node();
+			temp = head;
+			for(int i = 0; i < pos-1; i++){
+				temp = temp.getNext();
+			}
+			temp.setNext(node);			
+			node.setNext(null);
+		}
+		return head;
+	}
+	
+	public static void print(Node head){
+		
+		Node temp = new Node();
+		temp = head;
+		while(temp.getNext()!=null){
+			temp = temp.getNext();
+			System.out.print(temp.getData() + " ");
+		}
+	}
+	
+}
+
+public class InsertList {
+
+	public static void main(String[] args) {
+		Node head = new Node();
+		Node node1 = Node.create(15);
+		head = Node.insert(head, node1, 1);
+		Node node2 = Node.create(10);
+		head = Node.insert(head, node2, 2);
+		Node node3 = Node.create(25);
+		head = Node.insert(head, node3, 3);
+		Node node4 = Node.create(45);
+		head = Node.insert(head, node4, 4);
+		Node node5 = Node.create(55);
+		head = Node.insert(head, node5, 5);
+		Node.print(head);
+	}
+
+}
