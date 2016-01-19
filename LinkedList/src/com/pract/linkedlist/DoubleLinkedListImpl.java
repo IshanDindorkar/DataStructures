df6802bdcca66c4e1de0c7225ff8ae290f5c83df
Ishan Dindorkar <ishan.dindorkar@yahoo.com>
2016-01-18 23:32:31 -0600

    Added deletion functionality in double linked list implementation

--- LinkedList/src/com/pract/linkedlist/DoubleLinkedListImpl.java
+++ LinkedList/src/com/pract/linkedlist/DoubleLinkedListImpl.java
@@ -32,6 +32,26 @@ public class DoubleLinkedListImpl {
 			
 	}
 	
+	public static DoubleLinkedListNode deleteNode(DoubleLinkedListNode head, int data){
+		
+		DoubleLinkedListNode temp = head;
+		DoubleLinkedListNode prev = null;
+		DoubleLinkedListNode next = null;
+		temp = head.getNext();
+		while(temp!=null){
+			prev = temp.getPrev();
+			next = temp.getNext();
+			if(temp.getData() == data){
+				prev.setNext(next);
+				next.setPrev(prev);
+			}
+			temp = temp.getNext();
+		}
+		
+		return head;
+		
+	}
+	
 	public static void print(DoubleLinkedListNode head){
 		
 		DoubleLinkedListNode temp = new DoubleLinkedListNode();
@@ -47,10 +67,23 @@ public class DoubleLinkedListImpl {
 		DoubleLinkedListNode node1 = new DoubleLinkedListNode(5);
 		DoubleLinkedListNode node2 = new DoubleLinkedListNode(10);
 		DoubleLinkedListNode node3 = new DoubleLinkedListNode(2);
+		DoubleLinkedListNode node4 = new DoubleLinkedListNode(14);
+		DoubleLinkedListNode node5 = new DoubleLinkedListNode(22);
+		DoubleLinkedListNode node6 = new DoubleLinkedListNode(1);
+		DoubleLinkedListNode node7 = new DoubleLinkedListNode(7);
+		DoubleLinkedListNode node8 = new DoubleLinkedListNode(18);
 		DoubleLinkedListNode head = new DoubleLinkedListNode();
 		head = DoubleLinkedListImpl.insertNode(head, node1);
 		head = DoubleLinkedListImpl.insertNode(head, node2);
 		head = DoubleLinkedListImpl.insertNode(head, node3);
+		head = DoubleLinkedListImpl.insertNode(head, node4);
+		head = DoubleLinkedListImpl.insertNode(head, node5);
+		head = DoubleLinkedListImpl.insertNode(head, node6);
+		head = DoubleLinkedListImpl.insertNode(head, node7);
+		head = DoubleLinkedListImpl.insertNode(head, node8);
+		DoubleLinkedListImpl.print(head);
+		System.out.println("\n After deleting ....");
+		DoubleLinkedListImpl.deleteNode(head, 10);
 		DoubleLinkedListImpl.print(head);
 	}
 
